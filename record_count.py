089887c26f27422fbe39a110035ed4300521c665
mews <mews@mews-base.com>
2020-02-18 17:26:59 +0900

    fix

--- record_count.py
+++ record_count.py
@@ -7,6 +7,8 @@ from PIL import Image
 import numpy as np
 import cv2
 
+from collections import defaultdict
+
 def usage():
     print('Usage: ' + sys.argv[0] + ' <records_dir_path>')
 
@@ -19,20 +21,24 @@ if __name__ == '__main__' :
     input_path = sys.argv[1]
     
     files = glob.glob(input_path + '/*.tfrecord')
+    objs = defaultdict(int)
     if len(files) == 0:
         print('invalid input path : ' + input_path)
         quit(-1)
     print('input files  : {}'.format(input_path + '/*.tfrecord'))
+
     
-    
-    object_counts = 0
     for record_file in files:
         record_iterator = tf.python_io.tf_record_iterator(record_file)
         
         for string_record in record_iterator:
             example = tf.train.Example()
             example.ParseFromString(string_record)
-            xmaxs = example.features.feature["image/object/bbox/xmax"].float_list.value
-            object_counts += len(xmaxs)
+            labels = example.features.feature["image/object/class/label"].int64_list.value
+
+            for label in labels:
+                objs[label] += 1
     
-    print('{} count : {}'.format(input_path, object_counts))
+    print(" --- object count --- ")
+    for k, v in objs.items() :
+        print("{} : {} ".format(k,v))
